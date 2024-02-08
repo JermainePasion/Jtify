a0d8aa689001667133e885ec8c3b906e9273ae9f
vinuya123 <ccvinuya@student.hau.edu.ph>
2024-02-08 23:13:07 +0800

    Sidebar Music Bar

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,23 +1,115 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../Navbar';
 import { listSongs } from '../../actions/songActions';
 import Song from '../Song';
+import { FaPlay, FaPause, FaStepForward, FaStepBackward } from 'react-icons/fa';
 
 function Home() {
   const dispatch = useDispatch();
   const { loading, error, songs } = useSelector(state => state.songList);
   const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [resumePlay, setResumePlay] = useState(false);
+  const audioRef = useRef(new Audio());
+  const progressBarRef = useRef(null);
   
   useEffect(() => {
     dispatch(listSongs());
   }, [dispatch]);
 
+  useEffect(() => {
+    audioRef.current.addEventListener('timeupdate', () => {
+      if (!isDragging) {
+        setCurrentTime(audioRef.current.currentTime);
+      }
+    });
+    audioRef.current.addEventListener('durationchange', () => {
+      setDuration(audioRef.current.duration);
+    });
+    return () => {
+      audioRef.current.removeEventListener('timeupdate', () => {
+        if (!isDragging) {
+          setCurrentTime(audioRef.current.currentTime);
+        }
+      });
+      audioRef.current.removeEventListener('durationchange', () => {
+        setDuration(audioRef.current.duration);
+      });
+    };
+  }, [isDragging]);
+
   const playSong = (song) => {
-    if (currentlyPlaying === song) {
-      setCurrentlyPlaying(null);
+    if (currentlyPlaying === song && !audioRef.current.paused) {
+      pauseSong();
     } else {
+      audioRef.current.src = song.file;
+      if (resumePlay) {
+        audioRef.current.currentTime = currentTime;
+      }
+      audioRef.current.play();
       setCurrentlyPlaying(song);
+      setResumePlay(false);
+    }
+  };
+
+  const pauseSong = () => {
+    audioRef.current.pause();
+    setResumePlay(true);
+  };
+
+  const togglePlayPause = () => {
+    if (!currentlyPlaying || audioRef.current.paused) {
+      if (!currentlyPlaying) {
+        playSong(songs[0]); // Start playing the first song if none is currently playing
+      } else {
+        playSong(currentlyPlaying);
+      }
+    } else {
+      pauseSong();
+    }
+  };
+
+  const skipTrack = (forward = true) => {
+    const currentIndex = songs.findIndex(song => song === currentlyPlaying);
+    let newIndex = currentIndex + (forward ? 1 : -1);
+    if (newIndex < 0) {
+      newIndex = songs.length - 1;
+    } else if (newIndex >= songs.length) {
+      newIndex = 0;
+    }
+    playSong(songs[newIndex]);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const handleTimeBarClick = (e) => {
+    const clickedPosition = e.clientX - progressBarRef.current.getBoundingClientRect().left;
+    const timePerPixel = duration / progressBarRef.current.offsetWidth;
+    const newCurrentTime = clickedPosition * timePerPixel;
+    audioRef.current.currentTime = newCurrentTime;
+    setCurrentTime(newCurrentTime);
+  };
+
+  const handleTimeBarMouseDown = () => {
+    setIsDragging(true);
+  };
+
+  const handleTimeBarMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const calculateTimeBarWidth = () => {
+    if (duration) {
+      return (currentTime / duration) * 100 + '%';
+    } else {
+      return '0%';
     }
   };
 
@@ -46,27 +138,56 @@ function Home() {
               <Song key={song.id} song={song} playSong={playSong} isPlaying={currentlyPlaying === song} />
             ))
           )}
-      </div>
-
-        {currentlyPlaying && (
-          <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', backgroundColor: 'black', color: '#fff', boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)' }}>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px' }}>
+        </div>
+        <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', backgroundColor: '#282828', color: '#fff', boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px', borderTop: '1px solid #535353' }}>
+            {currentlyPlaying && (
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src={currentlyPlaying.picture} alt="Album Art" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                <img src={currentlyPlaying.picture} alt="Album Art" style={{ width: '64px', height: '64px', marginRight: '20px', borderRadius: '4px' }} />
                 <div>
-                  <p style={{ margin: 0, fontWeight: 'bold', fontFamily: 'Verdana', fontSize: '14px' }}>{currentlyPlaying.name}</p>
-                  <p style={{ margin: 0, fontSize: '0.8em' }}>{currentlyPlaying.artist}</p>
+                  <p style={{ margin: 0, fontWeight: 'bold', fontFamily: 'Helvetica Neue', fontSize: '16px' }}>{currentlyPlaying.name}</p>
+                  <p style={{ margin: 0, fontSize: '14px', color: '#b3b3b3' }}>{currentlyPlaying.artist}</p>
                 </div>
               </div>
-              <audio controls autoPlay style={{ width: '50%', maxWidth: '300px', margin: '0 10px' }}>
-                <source src={currentlyPlaying.file} type="audio/mpeg" />
-                Your browser does not support this audio player.
-              </audio>
+            )}
+            <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
+              <button onClick={() => skipTrack(false)} style={{ backgroundColor: 'transparent', border: 'none', fontSize: '24px', color: '#fff', marginRight: '20px' }}>
+                <FaStepBackward />
+              </button>
+              <button onClick={togglePlayPause} style={{ backgroundColor: 'transparent', border: 'none', fontSize: '32px', color: '#fff', marginRight: '20px' }}>
+                {currentlyPlaying && !audioRef.current.paused ? <FaPause /> : <FaPlay />}
+              </button>
+              <button onClick={() => skipTrack()} style={{ backgroundColor: 'transparent', border: 'none', fontSize: '24px', color: '#fff', marginRight: '20px' }}>
+                <FaStepForward />
+              </button>
+            </div>
+            {duration > 0 && (
+              <div 
+                ref={progressBarRef} 
+                style={{ 
+                  position: 'absolute', 
+                  bottom: '10px',
+                  left: 0, 
+                  height: '4px', 
+                  backgroundColor: '#535353', 
+                  width: '100%',
+                  cursor: 'pointer',
+                  margin: 'auto'
+                }}
+                onClick={handleTimeBarClick}
+                onMouseDown={handleTimeBarMouseDown}
+                onMouseUp={handleTimeBarMouseUp}
+              >
+                <div style={{ height: '100%', width: calculateTimeBarWidth(), backgroundColor: '#1DB954' }} />
+              </div>
+            )}
+            <div style={{ color: '#b3b3b3', fontSize: '14px', minWidth: '50px', marginLeft: '5px', textAlign: 'center' }}>
+              {formatTime(currentTime)} / {formatTime(duration)}
             </div>
           </div>
-        )}
         </div>
       </div>
+    </div>
   );
 }
 
